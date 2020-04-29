75e511bb50577fce85c0ca3ee1f9c4dd65b253a5
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-29 13:39:57 +0200

    [OPP-276] add db-indexes

--- src/main/resources/db/migration/V1_1__create.sql
+++ src/main/resources/db/migration/V1_1__create.sql
@@ -4,3 +4,6 @@ CREATE TABLE draft(
     context     JSONB                       NOT NULL,
     created     TIMESTAMP   DEFAULT NOW()   NOT NULL
 );
+
+CREATE INDEX draft_owner_idx ON draft USING HASH(owner);
+CREATE INDEX draft_context_idx ON draft USING GIN(context);
