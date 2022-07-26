fdf37cd2f541c814ffc92b1780740abb6193a218
Nicklas Utgaard <nutgaard@gmail.com>
2022-07-26 11:04:54 +0200

    [KAIZEN-0] bump common
    logge feil ved uthenting av oidc-well-known url

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ val ktorVersion = "2.0.3"
 val prometheusVersion = "1.9.0"
 val logbackVersion = "1.2.11"
 val logstashVersion = "7.2"
-val modiaCommonVersion = "1.2022.07.26-08.21-d0b037103f82"
+val modiaCommonVersion = "1.2022.07.26-11.02-80688b0163cf"
 
 plugins {
     kotlin("jvm") version "1.7.10"
