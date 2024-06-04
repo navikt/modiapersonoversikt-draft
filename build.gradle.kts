28e0bc4da3abc713a10bd800568fbd8ae748dbba
Ludvig Hozman <ludvig123@gmail.com>
2024-06-04 15:39:38 +0200

    Oppgrader til java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,11 +79,11 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.test {
